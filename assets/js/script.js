8c93baecca6dda179005bce49db5cd71313480ad
BenWofford <woffordbt@gmail.com>
2021-09-02 17:36:11 -0400

    built out table and started buttons

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* var addButton = function() {
+    let button = document.getElementById("saveBtn");
+    let tableRow = document.getElementsByName("<tr>");
+    button.append(tableRow);
+};
+
+addButton();*/
