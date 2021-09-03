a3199c767837fbd51cd3d2a72262db01793a49d2
BenWofford <woffordbt@gmail.com>
2021-09-02 21:37:19 -0400

    fixed things in office hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-/* var addButton = function() {
-    let button = document.getElementById("saveBtn");
-    let tableRow = document.getElementsByName("<tr>");
-    button.append(tableRow);
-};
+$(document).ready(function() {
 
-addButton();*/
+    $(".saveBtn").on("click", function() {
+
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr("id");
+    
+        console.log(value, time);
+    
+        localStorage.setItem(time, value);
+    
+    });
+
+}); // Create element to check for time and refresh CSS/colors etc
+    //moment.js
+
+$("#hour-9 .description").val(localStorage.getItem('hour-9')); // repeat!
