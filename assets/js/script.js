822d10fb5e706f1f14d796ae04ab37537ba331a0
BenWofford <woffordbt@gmail.com>
2021-09-05 16:09:51 -0400

    working on time/color coding function, still not done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-let now = moment();
-
+// This sets the date in the Jumbotron
 $(document).add(function() {
     $("#currentDay").text(moment().format('MMMM Do YYYY'))
  });
+
 
 $(document).ready(function() {
 
@@ -18,30 +18,75 @@ $(document).ready(function() {
 
 }); 
 
-// Create element to check for time and refresh CSS/colors etc
+var checkHour = [
+    'hour-9',
+    'hour-10',
+    'hour-11',
+    'hour-12',
+    'hour-13',
+    'hour-14',
+    'hour-15',
+    'hour-16',
+    'hour-17',
+];
 
-//if (now() <= ('9:59:59 am').css("#hour-9", '.present')) X 1.0 BAD X
-
-/* $(document).ready(function() {
-    if ($(now()).is(<= "9:59:59 AM"))
-        $("#hour-9").css(".present")
-    else $("hour-9").css(".past")
-}); 2.0 WIP */
-
-/* $(document).ready(function() {
-    if ($(now()>09 && now()<09.59)) {
+$(document).ready(function() {
+    if ($(checkHour)<moment()) {
+        $('time-block').css(".past");
+    if ($(checkHour)=moment()) {
+        $('time-block').css(".present");
+    } else ($(checkHour)>moment())
+        $('time-block').css(".future");
+    };
+});
+/* These functions check for time and refresh CSS colors --> need fix, close!
+$(document).ready(function() {
+    if ($(moment()>09 && moment()<09,59)) {
         $("#hour-9").css(".present");
-    else
+    } else {
         $("hour-9").css(".past");
     }
-}); -- 3.0 WIP, broke, need fix :(  */
+    console.log(moment());
+});
 
+$(document).ready(function() {
+    if ($(moment()>10 && moment()<10,59)) {
+        $("#hour-10").css(".present");
+    } if ($(moment()<10)) {
+        $("#hour-10").css(".future");
+    }    
+    else {
+        $("hour-10").css(".past");
+    }
+});
+
+$(document).ready(function() {
+    if ($(moment()>11 && moment()<11,59)) {
+        $("#hour-11").css(".present");
+    } if ($(moment()<11)) {
+        $("#hour-11").css(".future");
+    }    
+    else {
+        $("hour-11").css(".past");
+    }
+}); */
+
+// This saves the input of the description fields into local storage
 $("#hour-9 .description").val(localStorage.getItem('hour-9'));
+// console.log('hour-9');
 $("#hour-10 .description").val(localStorage.getItem('hour-10'));
+// console.log('hour-10');
 $("#hour-11 .description").val(localStorage.getItem('hour-11'));
+// console.log('hour-11');
 $("#hour-12 .description").val(localStorage.getItem('hour-12'));
+// console.log('hour-12');
 $("#hour-1 .description").val(localStorage.getItem('hour-1'));
+// console.log('hour-1');
 $("#hour-2 .description").val(localStorage.getItem('hour-2'));
+// console.log('hour-2');
 $("#hour-3 .description").val(localStorage.getItem('hour-3'));
+// console.log('hour-3');
 $("#hour-4 .description").val(localStorage.getItem('hour-4'));
+// console.log('hour-4');
 $("#hour-5 .description").val(localStorage.getItem('hour-5'));
+// console.log('hour-5');
